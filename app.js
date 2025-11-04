ade592d528eb52c2e8c7254ab766acc65a8b4084
SmoothTurtle872 <whatleyfamily1@gmail.com>
2025-11-05 09:05:22 +0930

    Added rectangle generator function
    Added an untested rectangle generator

--- app.js
+++ app.js
@@ -157,3 +157,167 @@ function generateCircle(particle, radius, particleCount, axis, direction, mode, 
     return particles;
 }
 
+const generateRectangle = (particle, size, particleCount, axis, direction, mode, motion, velocity) => {
+    if(!particle) {particle = "minecraft:end_rod"}
+    if(!size) {size = [5,5]}
+    if(!particleCount) {particleCount = 20}
+    if(!velocity) {velocity = 1}
+
+    let particles = "";
+
+    let symbol = "~";
+            
+
+    if(mode == "local") {
+        symbol = "^";
+    }
+
+    let perimeterSize = 2*size[0] + 2*size[1]
+    let widthPercent = size[0] / perimeterSize
+    let heightPercent = size[1] / perimeterSize
+    let widthParticles = Math.floor(particleCount * widthPercent)
+    let heightParticles = Math.floor(particleCount * heightPercent)
+    if (widthParticles < 1){
+        widthParticles = 1
+        heightParticles -= 1
+    }
+    if (heightParticles < 1){
+        heightParticles = 1
+        widthParticles -= 1
+    }
+    let side = 0
+    for (let i = 0; i < particleCount; i++){
+        let x = 0
+        let y = 0
+        let z = 0
+
+        if (axis == "horizontal"){
+            if (side == 0){
+                z = -size[1] / 2
+                x = (-size[0] / 2) + (i / widthParticles)
+                if (i == widthParticles){
+                    side = 1
+                }
+            }
+            if (side == 1){
+                z = size[1] / 2
+                x = (-size[0] / 2) + ((i - widthParticles) / widthParticles)
+                if ((i - widthParticles) == widthParticles){
+                    side = 2
+                }
+            }
+            if (side == 2){
+                x = -size[0] / 2
+                z = (-size[1] / 2) + ((i - (2 * widthParticles)) / heightParticles)
+                if (i == widthParticles){
+                    side = 3
+                }
+            }
+            if (side == 3){
+                x = size[0] / 2
+                z = (-size[1] / 2) + ((i - ((2 * widthParticles) + heightParticles)) / heightParticles)
+            }
+        } else{
+            if (mode == "world" && direction == "z"){
+                if (side == 0){
+                    y = -size[1] / 2
+                    x = (-size[0] / 2) + (i / widthParticles)
+                    if (i == widthParticles){
+                        side = 1
+                    }
+                }
+                if (side == 1){
+                    y = size[1] / 2
+                    x = (-size[0] / 2) + ((i - widthParticles) / widthParticles)
+                    if ((i - widthParticles) == widthParticles){
+                        side = 2
+                    }
+                }
+                if (side == 2){
+                    x = -size[0] / 2
+                    y = (-size[1] / 2) + ((i - (2 * widthParticles)) / heightParticles)
+                    if (i == widthParticles){
+                        side = 3
+                    }
+                }
+                if (side == 3){
+                    x = size[0] / 2
+                    y = (-size[1] / 2) + ((i - ((2 * widthParticles) + heightParticles)) / heightParticles)
+                }
+            } else {
+                if (side == 0){
+                    y = -size[1] / 2
+                    z = (-size[0] / 2) + (i / widthParticles)
+                    if (i == widthParticles){
+                        side = 1
+                    }
+                }
+                if (side == 1){
+                    y = size[1] / 2
+                    z = (-size[0] / 2) + ((i - widthParticles) / widthParticles)
+                    if ((i - widthParticles) == widthParticles){
+                        side = 2
+                    }
+                }
+                if (side == 2){
+                    z = -size[0] / 2
+                    y = (-size[1] / 2) + ((i - (2 * widthParticles)) / heightParticles)
+                    if (i == widthParticles){
+                        side = 3
+                    }
+                }
+                if (side == 3){
+                    z = size[0] / 2
+                    y = (-size[1] / 2) + ((i - ((2 * widthParticles) + heightParticles)) / heightParticles)
+                }
+            }
+        }
+        const invertedMotionTypes = ["inwards", "downwards", "left"];
+        const distanceMotionTypes = ["inwards", "upwards", "downwards", "left", "right"];
+
+        if(motion == "still") {
+            particles += `particle ${particle} ${symbol}${x} ${symbol}${y} ${symbol}${z} 0 0 0 0 1\n`   
+        } else {
+            let motionX = "";
+            let motionY = "";
+            let motionZ = "";
+            let motionDirection = "";
+            let motionDistance = "";
+            if(distanceMotionTypes.includes(motion)) {
+                motionDistance = radius;
+            }
+
+            let yaw = "~";
+            let pitch = "~";
+            if(axis == "horizontal") {
+                yaw = angle;
+                pitch = "0";
+            } else if(axis == "vertical") {
+                pitch = angle;
+            }
+
+            if(motion == "outwards" || motion == "inwards") {
+                motionZ = 1000000;
+                if(invertedMotionTypes.includes(motion)) {
+                    motionZ = "-" + motionZ;
+                }
+            } else if(motion == "upwards" || motion == "downwards") {
+                motionY = 1000000
+                if(invertedMotionTypes.includes(motion)) {
+                    motionY = "-" + motionY;
+                }
+            } else if(motion == "left" || motion == "right") {
+                motionX = 1000000
+                if(invertedMotionTypes.includes(motion)) {
+                    motionX = "-" + motionX;
+                }
+            }
+            particles += `execute rotated ${yaw} ${pitch} positioned ^ ^ ^${motionDistance} run particle ${particle} ~ ~ ~ ^${motionX} ^${motionY} ^${motionZ} ${(0.0000001 * velocity).toFixed(8)} 0\n`
+
+                       
+        }
+ 
+    }
+    return particles
+
+}
